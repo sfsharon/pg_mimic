e416cf7bba5c9c1b3236b46632c16703787bf7b8
Sharon Fraiman <sharonf@sqreamtech.com>
2021-10-28 17:27:48 +0300

    Implement B_Msg_Query_Deserialize

--- pg_serdes.py
+++ pg_serdes.py
@@ -42,6 +42,13 @@ PARSE_MSG__STATEMENT = "statement"
 PARSE_MSG__QUERY     = "query"
 PARSE_MSG__PARAMETER = "parameter"
 
+BIND_MSG__STATEMENT         = "statement"
+BIND_MSG__PORTAL            = "portal"
+BIND_MSG__PARAM_FORMATS     = "param_formats"
+BIND_MSG__PARAM_VALUES      = "param_values"
+BIND_MSG__RESULT_FORMATS    = "result_formats"
+BIND_MSG__FORMAT_TYPES      = "format_type"
+
 # Misc
 NULL_TERMINATOR = b'\x00'
 
@@ -184,12 +191,6 @@ def Q_Msg_Query_Deserialize(data) :
     @return query string
 
     Query (Frontend)
-        # Byte1('Q')
-        # Identifies the message as a simple query.
-
-        # Int32
-        # Length of message contents in bytes, including self.
-
         String
         The query string itself.        
     """
@@ -198,9 +199,7 @@ def Q_Msg_Query_Deserialize(data) :
     logging.info("*** Q_Msg_Query_Deserialize: Query received \"{}\"".format(query))
 
     return query
-# ---------------------------
-# WIP
-# ---------------------------
+
 def P_Msg_Query_Deserialize(data) :
     """! Deserialize Parse message
     @param data bytes array of the simple query
@@ -208,11 +207,6 @@ def P_Msg_Query_Deserialize(data) :
     @return Parse 
 
     Parse (Frontend)
-    # Byte1('P')
-    # Identifies the message as a Parse command.
-
-    # Int32
-    # Length of message contents in bytes, including self.
 
     String
     The name of the destination prepared statement (an empty string selects the unnamed prepared statement).
@@ -233,7 +227,7 @@ def P_Msg_Query_Deserialize(data) :
 
     parsed_msg = {}
 
-    assert (msg_id == PARSE_MSG_ID, f"Received {msg_id} unexpected message ID")
+    assert (msg_id == PARSE_MSG_ID, f"Received '{msg_id}' unexpected message ID")
 
     statement = payload[ : payload.find(NULL_TERMINATOR) + 1]
     parsed_msg[PARSE_MSG__STATEMENT] = statement
@@ -246,6 +240,74 @@ def P_Msg_Query_Deserialize(data) :
     PAYLOAD_STRUCT = "!h"     
     parameter = struct.unpack(PAYLOAD_STRUCT, payload[0:struct.calcsize(PAYLOAD_STRUCT)])
     parsed_msg[PARSE_MSG__PARAMETER] = parameter[0]
+
+    return parsed_msg
+
+def B_Msg_Query_Deserialize(data) :
+    """! Deserialize Bind message
+    @param data bytes array 
+
+    @return  
+
+    Bind (Frontend)
+        String
+        The name of the destination portal (an empty string selects the unnamed portal).
+
+        String
+        The name of the source prepared statement (an empty string selects the unnamed prepared statement).
+
+        Int16
+        The number of parameter format codes that follow (denoted C below). This can be zero to indicate that there are 
+        no parameters or that the parameters all use the default format (text); 
+        or one, in which case the specified format code is applied to all parameters; 
+        or it can equal the actual number of parameters.
+
+        Int16[C]
+        The parameter format codes. Each must presently be zero (text) or one (binary).
+
+        Int16
+        The number of parameter values that follow (possibly zero). This must match the number of parameters needed by the query.
+
+        Next, the following pair of fields appear for each parameter:
+
+        Int32
+        The length of the parameter value, in bytes (this count does not include itself). Can be zero. As a special case, 
+        -1 indicates a NULL parameter value. No value bytes follow in the NULL case.
+
+        Byten
+        The value of the parameter, in the format indicated by the associated format code. n is the above length.
+
+        After the last parameter, the following fields appear:
+
+        Int16
+        The number of result-column format codes that follow (denoted R below). This can be zero to indicate that there 
+        are no result columns or that the result columns should all use the default format (text); 
+        or one, in which case the specified format code is applied to all result columns (if any); or it can equal the actual number of result columns of the query.
+
+        Int16[R]
+        The result-column format codes. Each must presently be zero (text) or one (binary). 
+    """
+    msg_id = data[0]
+    payload = data[1]
+
+    parsed_msg = {}
+
+    assert (msg_id == BIND_MSG_ID, f"Received '{msg_id}' unexpected message ID")
+
+    portal = payload[ : payload.find(NULL_TERMINATOR) + 1]
+    parsed_msg[BIND_MSG__PORTAL] = portal
+    payload = payload[len(portal) : ]
+
+    statement = payload[ : payload.find(NULL_TERMINATOR) + 1]
+    parsed_msg[BIND_MSG__STATEMENT] = statement
+    payload = payload[len(statement) : ]
+
+    PAYLOAD_STRUCT = "!hhhh"     
+    param_formats, param_values, result_formats, format_type = struct.unpack(PAYLOAD_STRUCT, payload[0:struct.calcsize(PAYLOAD_STRUCT)])
+    parsed_msg[BIND_MSG__PARAM_FORMATS]  = param_formats
+    parsed_msg[BIND_MSG__PARAM_VALUES]   = param_values
+    parsed_msg[BIND_MSG__RESULT_FORMATS] = result_formats
+    parsed_msg[BIND_MSG__FORMAT_TYPES]   = format_type
 
     return parsed_msg
 
